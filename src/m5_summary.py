b7ec1cd2dcb9c24db31b44b83cd04c19a0324b6c
sparksac <sparksac@rose-hulman.edu>
2018-03-05 10:44:06 -0500

    Test

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Anthony Sparks.
 """
 ########################################################################
-# TODO: 1.
+# Done.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -27,3 +27,9 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+window.close_on_mouse_click()
