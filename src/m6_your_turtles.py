856abe603d32e25cb7e71d3fd60615e2c507d236
sparksac <sparksac@rose-hulman.edu>
2018-03-05 11:12:34 -0500

    Completed

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Anthony Sparks.
 """
 ###############################################################################
-# TODO: 1.
+# Done.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
@@ -27,3 +27,50 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+anthony = rg.SimpleTurtle()
+anthony.pen.color = 'blue'
+anthony.pen.thickness = 2
+anthony.speed = 20
+
+size = 300
+
+
+for k in range(10):
+
+    anthony.draw_square(size)
+
+    anthony.pen_up()
+    anthony.right(45)
+    anthony.forward(10)
+    anthony.left(45)
+
+    anthony.pen_down()
+    size = size - 12
+
+radius = 100
+
+blue = rg.SimpleTurtle('turtle')
+blue.pen.color = 'green'
+blue.pen.thickness = 2
+blue.speed = 20
+
+blue.left(180)
+blue.forward(100)
+blue.right(180)
+for k in range(10):
+
+    blue.draw_circle(radius)
+
+    blue.pen_up()
+    blue.left(90)
+    blue.forward(10)
+    blue.right(90)
+
+    blue.pen_down()
+    radius = radius - 10
+
+window.close_on_mouse_click()
