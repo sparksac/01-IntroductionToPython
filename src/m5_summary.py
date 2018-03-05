52b0fc681db716a2d6378005f279cb41406d4bb7
sparksac <sparksac@rose-hulman.edu>
2018-03-05 10:44:42 -0500

    Completed

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -10,7 +10,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done.
 #   Write code that constructs a SimpleTurtle with a red Pen
 #   and makes it move around a bit.  Don't forget to:
 #     -- import rosegraphics and construct a TurtleWindow
