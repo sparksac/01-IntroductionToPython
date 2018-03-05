1293408bc1eef44f52af7b8c75cbf7ced0d69871
sparksac <sparksac@rose-hulman.edu>
2018-03-05 10:33:40 -0500

    Completed

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -1,5 +1,6 @@
 ###############################################################################
 #
+
 #   Read the code below.  Predict (in your head) what will get printed
 #   (i.e., displayed) when the code runs.
 #
